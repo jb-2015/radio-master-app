a7719bc1d77fd19173b7b25aed567f8f3400c118
Juan Becerra <juan_matias_beerra@hotmail.com>
2026-05-03 16:08:09 -0300

    avance en diseño

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-const radio = document.getElementById("radio");
+//const radio = document.getElementById("radio");
 
-function playRadio() {
+/*function playRadio() {
   radio.play();
 }
 
 function stopRadio() {
   radio.pause();
-}
+}*/
 
 if ("serviceWorker" in navigator) {
   navigator.serviceWorker.register("service-worker.js");
