626dbf596449a5db737432feb089726c37690ab6
Juan Becerra <juan_matias_beerra@hotmail.com>
2026-05-08 19:57:40 -0300

    modificacion del boton de play

--- app.js
+++ app.js
@@ -8,6 +8,29 @@ function stopRadio() {
   radio.pause();
 }*/
 
+const btn = document.getElementById("playBtn");
+const radio = document.getElementById("radio");
+
+let reproduciendo = false;
+
+btn.addEventListener("click", () => {
+
+    if(!reproduciendo){
+
+        radio.play();
+        btn.innerHTML = "⏸";
+        reproduciendo = true;
+
+    }else{
+
+        radio.pause();
+        btn.innerHTML = "▶";
+        reproduciendo = false;
+
+    }
+
+});
+
 if ("serviceWorker" in navigator) {
   navigator.serviceWorker.register("service-worker.js");
 }
